9c89fac720c1bfc84d1775666443f1c5c15ab926
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-02-21 16:48:15 -0800

    1st, working through Meta's React training

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (props) => {
+
+   return (
+      <h1>Hello there indeed, {props.name}, {props.color}</h1>
+   )
+}
+
+export default Header
---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = (props) => {
+   return (
+      <h2>{props.greet} from Sidebar</h2>
+   )
+}
+
+export default Sidebar;
